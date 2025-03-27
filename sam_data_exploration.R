634b4329e9266bf3faf1a35cdb800f7299006a61
Samantha Jensen <samanthajensen@Samanthas-MacBook-Air-3.local>
2025-03-27 17:00:09 -0400

    commit trying again

---
+++ sam_data_exploration.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(here)
+
+exposure <- read.csv("ExposureConcentrationsRaw.csv")
+
+formaldehyde <- exposure %>% 
+  filter(Pollutant.Name == "FORMALDEHYDE") %>% 
+  select(c(County, Tract, Total.Exposure.Conc, OR.LightDuty.OffNetwork.Gas.Exposure.Conc:OR.HeavyDuty.Hoteling.Exposure.Conc))
