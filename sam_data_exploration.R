d33bd30e3a440c124cf75e2d38559369e56eff92
Samantha Jensen <samanthajensen@Samanthas-MacBook-Air-4.local>
2025-03-31 11:13:22 -0400

    saj commit 3/31

--- sam_data_exploration.R
+++ sam_data_exploration.R
@@ -1,62 +1,55 @@
 library(tidyverse)
 library(here)
+library(readxl)
 
 exposure <- read.csv("ExposureConcentrationsRaw.csv")
 
+urban_rural <- read_xlsx("urban_rural.xlsx")
+
 formaldehyde <- exposure %>% 
   filter(Pollutant.Name == "FORMALDEHYDE") %>% 
-  select(c(County, Tract, Total.Exposure.Conc, 
-  OR.LightDuty.OffNetwork.Gas.Exposure.Conc:OR.HeavyDuty.Hoteling.Exposure.Conc))
+  select(c(County, Population, Total.Exposure.Conc, 
+  OR.LightDuty.OffNetwork.Gas.Exposure.Conc:BACKGROUND.Exposure.Conc))
 
-onroad_formaldehyde <- formaldehyde %>% 
-  mutate(onroad_proportion = (OR.LightDuty.OffNetwork.Gas.Exposure.Conc + 
-                         OR.LightDuty.OffNetwork.Diesel.Exposure.Conc + 
-                         OR.HeavyDuty.OffNetwork.Gas.Exposure.Conc +
-                        OR.HeavyDuty.OffNetwork.Diesel.Exposure.Conc + 
-                        OR.LightDuty.OnNetwork.Gas.Exposure.Conc + 
-                        OR.LightDuty.OnNetwork.Diesel.Exposure.Conc + 
-                       OR.HeavyDuty.OnNetwork.Gas.Exposure.Conc +
-                       OR.HeavyDuty.OnNetwork.Diesel.Exposure.Conc + 
-                      OR.Refueling.Exposure.Conc + 
-                     OR.HeavyDuty.Hoteling.Exposure.Conc)/Total.Exposure.Conc)
-
-nonroad_form <- exposure %>% 
-  filter(Pollutant.Name == "FORMALDEHYDE") %>% 
-  select(c(County, Tract, Total.Exposure.Conc, 
-          NR.Recreational.inc.PleasureCraft.Exposure.Conc:NR..Point.Railyards.Exposure.Conc)) %>% 
-  mutate(non_road_proportion = (NR.Recreational.inc.PleasureCraft.Exposure.Conc + 
-           NR.Construction.Exposure.Conc + NR.CommercialLawnGarden.Exposure.Conc + 
-          NR.ResidentialLawnGarden.Exposure.Conc + NR.Agriculture.Exposure.Conc + 
-           NR.CommercialEquipment.Exposure.Conc + NR.AllOther.Exposure.Conc + 
-           NR.CMV_C1C2.Exposure.Conc + NR.CMV_C3.Exposure.Conc + 
-          NR.CMV_C1C2C3_underway.Exposure.Conc + NR.Locomotives.Exposure.Conc + 
-          NR.Point.Airports.Exposure.Conc + NR..Point.Railyards.Exposure.Conc)/ Total.Exposure.Conc)
-
-summary(nonroad_form$non_road_proportion)
-summary(onroad_formaldehyde$onroad_proportion)
-
-ggplot(nonroad_form, aes(x = non_road_proportion)) + 
-  geom_histogram()
-
-nonroad_bycounty <- nonroad_form %>% 
+formaldehyde <- formaldehyde %>% 
   group_by(County) %>% 
-  summarise(NR.Recreational.inc.PleasureCraft.Exposure.Conc = mean(NR.Recreational.inc.PleasureCraft.Exposure.Conc), 
-            NR.Construction.Exposure.Conc = mean(NR.Construction.Exposure.Conc), 
-            NR.CommercialLawnGarden.Exposure.Conc = mean(NR.CommercialLawnGarden.Exposure.Conc), 
-            NR.ResidentialLawnGarden.Exposure.Conc = mean(NR.ResidentialLawnGarden.Exposure.Conc), 
-            NR.Agriculture.Exposure.Conc = mean(NR.Agriculture.Exposure.Conc), 
-            NR.CommercialEquipment.Exposure.Conc = mean(NR.CommercialEquipment.Exposure.Conc), 
-            NR.AllOther.Exposure.Conc = mean(NR.AllOther.Exposure.Conc), 
-            NR.CMV_C1C2.Exposure.Conc = mean(NR.CMV_C1C2.Exposure.Conc), 
-            NR.CMV_C3.Exposure.Conc = mean(NR.CMV_C3.Exposure.Conc), 
-            NR.CMV_C1C2C3_underway.Exposure.Conc = mean(NR.CMV_C1C2C3_underway.Exposure.Conc), 
-            NR.Locomotives.Exposure.Conc = mean(NR.Locomotives.Exposure.Conc), 
-            NR.Point.Airports.Exposure.Conc = mean(NR.Point.Airports.Exposure.Conc), 
-            NR..Point.Railyards.Exposure.Conc = mean(NR..Point.Railyards.Exposure.Conc))
+  summarise(across(where(is.numeric), sum, na.rm = TRUE)) %>% 
+  ungroup()
+
+formaldehyde <- formaldehyde[-34,]
+
+formaldehyde_joined <- left_join(formaldehyde, urban_rural, by = "County")
+
+urban <- formaldehyde_joined %>% 
+  filter(URBAN_RURAL == "Urban")
+rural <- formaldehyde_joined %>% 
+  filter(URBAN_RURAL == "Rural")
+
+urban_rural_plot <- ggplot(formaldehyde_joined, aes(x = Total.Exposure.Conc, color = URBAN_RURAL)) +
+  geom_boxplot() 
+urban_rural_plot
+
+populationPlot <- ggplot(formaldehyde_joined, aes(x = Population, color = URBAN_RURAL)) + 
+  geom_histogram()
+populationPlot
+
+(exposureXpopulation <- ggplot(formaldehyde_joined, aes(x = Population, y = Total.Exposure.Conc, color = URBAN_RURAL)) + 
+  geom_point()) + 
+  geom_smooth(method = "lm")
+
+# Checking Normality 
+(urban_plot <- ggplot(urban, aes(x = Total.Exposure.Conc)) + 
+  geom_histogram(bins = 8))
+
+# checking Normality 
+(rural_plot <- ggplot(rural, aes(x = Total.Exposure.Conc)) + 
+  geom_histogram(bins = 8))
+
+# Running Mann Whitney U test because the samples aren't normally distributed (Can do this for multiple pollutants or grouping by acitivity)
+mann_whit_test <- wilcox.test(urban$Total.Exposure.Conc, rural$Total.Exposure.Conc, paired = FALSE)
+mann_whit_test
 
 
-nonroad_bycounty <- nonroad_bycounty[-34, ]
-### Need to subtract entire state row 
 
             
             
